bd46a7191fafa65b974071b55f57cd4e523d9847
609546135@qq.com <z609546135>
2018-09-16 18:13:56 +0800

    订单

--- pages/commodityDetails/commodityDetails.js
+++ pages/commodityDetails/commodityDetails.js
@@ -1,25 +1,22 @@
 // pages/commodityDetails/commodityDetails.js
+var { globalData} = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    data:{},
+    selectCommodity:{},
+    specifications:false,
+    quantity:1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    this.getData({ goodsId:options.id})
   },
 
   /**
@@ -42,25 +39,35 @@ Page({
   onUnload: function () {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  getData: function (params){ //获取信息
+    wx.http.postReq('appletClient?m=findClientBuildingMaterialsGoodsByIdInfo', params, (res) => {
+      let { success ,value}=res;
+      if (success) {
+        let selectCommodity = !value.modelList[0] ? {} : value.modelList[0];
+        selectCommodity.quantity=1;
+        this.setData({ 
+          data: value,
+          selectCommodity
+        });
+      }
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  handleSpecifications:function(){
+    let specifications = this.data.specifications;
+    this.setData({ specifications: !specifications});
   },
-
   /**
-   * 用户点击右上角分享
+   * 选择规格
    */
-  onShareAppMessage: function () {
-
+  changeSpecifications:function(e){
+    let itemdata = e.currentTarget.dataset.itemdata;
+    itemdata.quantity = this.quantity;
+    this.setData({ selectCommodity:itemdata});
+  },
+  purchase:function(){ //立即购买
+    global.selectCommodity = this.data.selectCommodity;
+    wx.navigateTo({
+      url: '../order-submit/submit?merchantId=' + this.data.data.merchantId
+    });
   }
 })

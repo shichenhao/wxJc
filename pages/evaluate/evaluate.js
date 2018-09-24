69805fae82e08e93b6fe1f0d9c68183f37e64e62
609546135@qq.com <z609546135>
2018-09-24 23:31:10 +0800

    订单评价

---
+++ pages/evaluate/evaluate.js
@@ -0,0 +1,55 @@
+// pages/evaluate/evaluate.js
+var { globalData } = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    widthS: 0,
+    widthC: 0,
+    valuateContent:''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  changeWidthS(e) {
+    let width = e.currentTarget.dataset.width;
+    this.setData({ widthS: width });
+  },
+  changeWidthC(e) {
+    let width = e.currentTarget.dataset.width;
+    this.setData({ widthC: width });
+  },
+  changeEvaluateContent(e){ //评价内容
+    this.setData({
+      valuateContent: e.detail.value
+    })
+  },
+  submit(){
+    let { widthS, widthC, valuateContent}=this.data;
+    let params={
+      orderId: globalData.orderDetail.id,
+      orderItemId: globalData.orderDetail.buildingMaterialsOrderItemList[0].id,
+      content: valuateContent,
+      goodsScore: widthC,
+      merchantScore: widthS
+    };
+    wx.http.postReq('appletClient?m=createBuildingMaterialsGoodsComments', params, (res) => {
+      let { success, value } = res;
+      if (success) {
+        this.setData({
+          addressData: value[0]
+        })
+      } else {
+        wx.showToast({
+          title: '出错了,请联系管理员',
+        })
+      }
+    })
+  }
+})
